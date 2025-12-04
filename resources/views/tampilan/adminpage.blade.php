e27c3a518a2c9cc51cf2e7ae90e0ae3bea764dad
kumahasiateh <zaid.net2018@gmail.com>
2025-12-04 14:15:02 +0700

    Admin Murka

--- resources/views/tampilan/adminpage.blade.php
+++ resources/views/tampilan/adminpage.blade.php
@@ -45,7 +45,7 @@
             @endif
             
             <div 
-                x-show="open" 
+                x-show="open" x-cloak
                 x-transition 
                 class="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
                 <div class="bg-white rounded-xl shadow-lg w-full max-w-md p-6 relative">
